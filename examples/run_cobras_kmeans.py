60ff48b4d5f1eff2093718232c1fc25407f9ba0f
LeersTijs <tijs.leers@student.kuleuven.be>
2023-11-02 13:31:16 +0100

    first test

--- examples/run_cobras_kmeans.py
+++ examples/run_cobras_kmeans.py
@@ -5,13 +5,24 @@ from cobras_ts.cobras_kmeans import COBRAS_kmeans
 from cobras_ts.querier.labelquerier import LabelQuerier
 
 
-budget = 100
+def convert_to_num(label):
+    match label:
+        case 'Iris-setosa':
+            return 0
+        case 'Iris-versicolor':
+            return 1
+        case 'Iris-virginica':
+            return 2
 
-data = np.loadtxt('/home/toon/data/iris.data', delimiter=',')
-X = data[:,1:]
-labels = data[:,0]
+
+budget = 10
+path = 'D:/School/2023-2024/thesis/dataSets/Iris/iris.data'
+X = np.loadtxt(path, delimiter=',', usecols=[0, 1, 2, 3])
+labels = np.genfromtxt(path, delimiter=',', dtype=str, usecols=[4])
+
+labels = list(map(convert_to_num, labels))
 
 clusterer = COBRAS_kmeans(X, LabelQuerier(labels), budget)
 clustering, intermediate_clusterings, runtimes, ml, cl = clusterer.cluster()
 
-print(metrics.adjusted_rand_score(clustering.construct_cluster_labeling(),labels))
+print(metrics.adjusted_rand_score(clustering.construct_cluster_labeling(), labels))
